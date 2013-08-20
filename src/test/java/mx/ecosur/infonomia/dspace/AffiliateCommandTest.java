f08233eeaf4ee5202902eecf8b4ba2ce9029971c
Andrew Waterman <andrew.waterman@gmail.com>
2013-08-20 16:17:16 -0500

    Added test files.

---
+++ src/test/java/mx/ecosur/infonomia/dspace/AffiliateCommandTest.java
@@ -0,0 +1,35 @@
+package mx.ecosur.infonomia.dspace;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* AffiliateCommand Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jun 7, 2013</pre> 
+* @version 1.0 
+*/ 
+public class AffiliateCommandTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: execute(Context context, Item item, Deposit deposit) 
+* 
+*/ 
+@Test
+public void testExecute() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ src/test/java/mx/ecosur/infonomia/dspace/ExtendCommandTest.java
@@ -0,0 +1,35 @@
+package mx.ecosur.infonomia.dspace; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* ExtendCommand Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jun 17, 2013</pre> 
+* @version 1.0 
+*/ 
+public class ExtendCommandTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: execute(Context context, Deposit deposit, Item item) 
+* 
+*/ 
+@Test
+public void testExecute() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
